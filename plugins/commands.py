a54be2f161fa02e32ebc3d3c42ce20def5ba3434
itseleven011 <79216932+itseleven011@users.noreply.github.com>
2021-05-25 17:57:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Movies Channel", url="https://t.me/Moviezzclub"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Main Channel", url="https://t.me/Moviezzclub"),
+                    InlineKeyboardButton("Request Group", url="https://t.me/movieezzclub")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/nosourcecode")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
