ff80fb3067ff5ce9cf7149d79ed6f4ccf115af94
itseleven011 <79216932+itseleven011@users.noreply.github.com>
2021-05-29 22:43:47 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
-                    InlineKeyboardButton("About Me", callback_data="about_data")
+                    InlineKeyboardButton("Want To request movie/series?", url="https:/t.me/movieezzclub"),
+                    InlineKeyboardButton("Dbaya Toh tu gadha", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Main Channel", url="https://t.me/Moviezzclub"),
+                    InlineKeyboardButton("Request Group", url="https://t.me/moviezzclub")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/nosourcecode")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
